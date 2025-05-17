9b7e81a8e516bf6e5ba9d156e4acbebf56299d43
dauda.dauda <dauda933@gmail.com>
2025-05-17 14:10:30 +0100

    edited prompt

--- backend/model/groq/translator.py
+++ backend/model/groq/translator.py
@@ -9,10 +9,13 @@ async def translate_text(text: str, lang: str = 'English', lang_to_translate_to:
         res = client.chat.completions.create(
             model="gemma2-9b-it",
             messages=[
-                {"role": "system", "content": f"Translate this {lang} text to {lang_to_translate_to}. if the text to be translated from {lang} to {lang_to_translate_to} doesnt make sense or is incoherent, just return the word 'incoherent'"},
+                {"role": "system", "content": f"Translate this {lang} text to {lang_to_translate_to}."},
                 {"role": "user", "content": text}
             ]
         )
         return res.choices[0].message.content.strip()
 
     return await asyncio.to_thread(sync_call)
+
+
+#  if the text to be translated from {lang} to {lang_to_translate_to} doesnt make sense or is incoherent, just return the word 'incoherent'
